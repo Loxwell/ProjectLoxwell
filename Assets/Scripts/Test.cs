87edea4cbb5d452f5272523c49586464803422ae
Loxwell <godfad3r@naver.com>
2020-06-07 12:59:00 +0900

    FIrst Commit

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D Other)
+    {
+	    UnityEngine.Debug.Log(Other.transform.name);
+    }
+
+}
