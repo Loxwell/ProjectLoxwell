183082c0e37a87b0865985c3bc1fd9beed3070f2
Loxwell <37389238+Loxwell@users.noreply.github.com>
2020-07-10 10:23:44 +0900

    HP 테스트 코드 등록

---
+++ Assets/Scripts/Test/TestHP.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TestHP : MonoBehaviour
+{
+
+    public Sprite[] heartOriginImages;
+    public Heart[] heartsImages;
+
+    int m_maxHP = 15;
+
+    public void ChangeHP(string  value)
+    {
+        int maxHp = m_maxHP / 4 + (m_maxHP % 4 != 0 ? 1 : 0 );
+
+        int hp = int.Parse(value);
+        int rest = (hp % 4);
+        int last = (hp - 1) / 4;
+
+        if (last >= heartsImages.Length)
+            last = heartsImages.Length - 1;
+
+        for (int i = 0; i < heartsImages.Length; ++i)
+        {
+            int imgIdx = i < last ? 4 : (i > last) ? 0 : (rest == 0) ? 4 : rest;
+            heartsImages[i].imgRenerer.enabled = (maxHp > i);
+            heartsImages[i].SetImage(heartOriginImages[imgIdx], i == last);
+        }
+    }
+
+    [System.Serializable]
+    public class Heart
+    {
+        [SerializeField]
+        public Animator animator;
+        [SerializeField]
+        public Image imgRenerer;
+
+
+        public void SetImage(Sprite newSprite, bool bPopup)
+        {
+            imgRenerer.sprite = newSprite;
+            if(bPopup)
+            {
+                animator.SetTrigger("ChangeState");
+            }
+        }
+    }
+}
